f65fef284993889d9ea47fa3ed031b5ecdc57760
69989 <konmat2005@gmail.com>
2026-02-02 23:44:24 +0100

    Commit 9, Zmiana modelu

--- app/page.tsx
+++ app/page.tsx
@@ -48,7 +48,7 @@ async function analyzeTextWithAI(text: string) {
     `;
 
     const response = await ai.models.generateContent({
-      model: "gemini-2.0-flash", 
+      model: "gemini-1.5-flash", 
       contents: prompt,
     });
     const responseText = response.text ? response.text.toString() : ''; 
